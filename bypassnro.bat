e9b0680cb787134007d0ad6d13fef7e2d0192a6a
bigboxofspam <64483460+bigboxofspam@users.noreply.github.com>
2025-12-09 16:47:04 -0600

    Update bypassnro.bat

--- bypassnro.bat
+++ bypassnro.bat
@@ -1,3 +1,2 @@
-@echo off
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/bigboxofspam/bypassnro/refs/heads/main/unattend.xml
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
+curl -L -o C:\Windows\Panther\unattend.xml https://github.com/bigboxofspam/bypassnro/blob/66d7c2cf59d8a04518417f68d390be84a7b76376/unattend.xml
+REM %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
