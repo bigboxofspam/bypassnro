66d7c2cf59d8a04518417f68d390be84a7b76376
bigboxofspam <64483460+bigboxofspam@users.noreply.github.com>
2025-12-09 16:41:32 -0600

    created

---
+++ bypassnro.bat
@@ -0,0 +1,3 @@
+@echo off
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/bigboxofspam/bypassnro/refs/heads/main/unattend.xml
+%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
